2f261de8f21b3c923bb3418a5f889ba7728024d4
Jimmy Douieb <jimmydouieb@gmail.com>
2018-07-31 00:17:34 +0300

    Add bin

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package.");
-}
+#! /usr/bin/env node
+
+console.log("This is a message from the demo package.");
+
+// exports.printMsg = function() {
+//   console.log("This is a message from the demo package");
+// }
